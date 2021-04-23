759aec83877e91c4858fb9fa2a7f5c9c0b5335b6
shwei <sh.wei.ds@outlook.com>
2021-04-23 11:33:22 +0800

    add line

--- utils/args.py
+++ utils/args.py
@@ -6,7 +6,7 @@ import argparse
 # Optional Datasets
 DATASETS = ['mnist', 'femnist', 'cifar10', 'cifar100']
 # Optional Algorithms
-MODELS = ['fedavg', 'fedprox', 'fednova']
+MODELS = ['fedavg', 'fedprox', 'fednova', 'add line']
 
 
 def parse_args():
